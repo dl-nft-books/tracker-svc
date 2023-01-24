3a44164a47ac154247ba363cc048ba9ded15542b
anastasiia.lishchenko <anastasiia.lishchenko@gmail.com>
2023-01-24 14:02:09 +0200

    Add unique constraint to key_value

--- internal/assets/migrations/006_add_networks.sql
+++ internal/assets/migrations/006_add_networks.sql
@@ -3,9 +3,10 @@
 ALTER TABLE contracts ADD COLUMN IF NOT EXISTS chain_id bigint NOT NULL DEFAULT 0;
 ALTER TABLE key_value
 DROP CONSTRAINT IF EXISTS key_value_key_key,
-ADD COLUMN IF NOT EXISTS chain_id int NOT NULL DEFAULT 0;
-
+ADD COLUMN IF NOT EXISTS chain_id int NOT NULL DEFAULT 0,
+ADD UNIQUE ( key, chain_id);
 -- +migrate Down
 ALTER TABLE contracts DROP COLUMN IF EXISTS chain_id;
-ALTER TABLE key_value DROP COLUMN IF EXISTS chain_id;
-
+ALTER TABLE key_value
+DROP COLUMN IF EXISTS chain_id,
+DROP CONSTRAINT IF EXISTS key_value_key_chain_id_key;
